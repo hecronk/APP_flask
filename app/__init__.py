94f512e345b93ef0c8a6a93bd4bb6dd00e300d7a
hecronk <damiraminov1@yandex.ru>
2022-03-14 13:06:27 +0500

    added errors

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,6 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
     return app
