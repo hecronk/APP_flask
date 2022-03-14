4adb043ceb75eeeabdcd8b9be579cd042e5c7989
hecronk <damiraminov1@yandex.ru>
2022-03-15 01:59:20 +0500

    added flashes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,6 +6,8 @@ from app.auth.forms import LoginForm, RegistrationForm
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
     return render_template('auth/login.html', title='Sign In', form=form)
 
 
