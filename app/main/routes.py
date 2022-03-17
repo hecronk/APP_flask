c6a301f2370cbb99851a683b8e54d6b1afd7e1e4
hecronk <damiraminov1@yandex.ru>
2022-03-17 16:44:22 +0500

    routes fixes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,14 @@
 from app.main import bp
 from flask import render_template
 from flask_login import login_required
+from app.models.user import User
 
 
+@bp.route('/feed', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
-def index():
+def feed():
     # temporary plug
     posts = [
         {
@@ -18,4 +20,15 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('main/index.html', title='Home page', posts=posts)
+    return render_template('main/feed.html', title='Home page', posts=posts)
+
+
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('main/user.html', user=user, posts=posts)
